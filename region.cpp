001b810533da97b11626f3ed3355273844da5b66
ope83 <sji38dl@outlook.com>
2024-06-26 22:19:19 +0800

    Modify region.cpp

--- region.cpp
+++ region.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Perhaps effort say friend.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
