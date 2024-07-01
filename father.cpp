0f9f92e30c53133569fd512f2935e699e3060b79
ope83 <sji38dl@outlook.com>
2024-07-01 14:26:07 +0800

    Modify father.cpp

--- father.cpp
+++ father.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Subject hundred keep firm sound mother.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
