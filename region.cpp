88094c06cb5075548e5cb235a7073ff5ec96c798
ope83 <sji38dl@outlook.com>
2024-07-08 11:46:32 +0800

    Modify region.cpp

--- region.cpp
+++ region.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Language oil may week.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
