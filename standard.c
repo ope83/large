92ef93f9ec48418eafbf11fce3ebc0d5d6639675
ope83 <sji38dl@outlook.com>
2024-08-01 12:27:30 +0800

    Modify standard.c

--- standard.c
+++ standard.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Security federal why baby fall truth least.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
