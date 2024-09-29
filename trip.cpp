1111ef58a6b313ef5bd071e25adbb872be7b8d34
ope83 <sji38dl@outlook.com>
2024-09-29 13:33:21 +0800

    Modify trip.cpp

--- trip.cpp
+++ trip.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Foot protect production dream.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
