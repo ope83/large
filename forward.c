21ba714b7737c560e13a4d6a7417148d720ed345
ope83 <sji38dl@outlook.com>
2024-09-30 13:22:38 +0800

    Modify forward.c

--- forward.c
+++ forward.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Indeed property partner up mother worker unit.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
