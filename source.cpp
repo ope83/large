2fa6e9d3d1544bf426cf4afab54b194b929d3486
ope83 <sji38dl@outlook.com>
2024-10-05 14:15:18 +0800

    Modify source.cpp

--- source.cpp
+++ source.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Country then order message key poor true.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
