6ce83780fb774a26281cf9219e8b324f61873052
ope83 <sji38dl@outlook.com>
2024-11-07 00:46:08 +0800

    Modify practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Trial final democratic talk.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
