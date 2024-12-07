c8515583379d541de475454afdc365a11ed49a27
ope83 <sji38dl@outlook.com>
2024-12-07 20:33:22 +0800

    Modify just.c

--- just.c
+++ just.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Service story more available treatment successful particularly.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
