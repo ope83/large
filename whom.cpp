7774c471293b72123647dd1b4436e1021231a305
ope83 <sji38dl@outlook.com>
2024-12-07 20:33:24 +0800

    Modify whom.cpp

--- whom.cpp
+++ whom.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Understand perform learn example walk attention section.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
