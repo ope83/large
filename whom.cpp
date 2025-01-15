c3c56ffd31906df79f70af9f3acbcaad682a371b
ope83 <sji38dl@outlook.com>
2025-01-15 12:05:10 +0800

    Modify whom.cpp

--- whom.cpp
+++ whom.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Likely series first tough.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
