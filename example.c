af00d37744b6a42942baa606487800c3bde0f5b3
ope83 <sji38dl@outlook.com>
2025-01-23 13:17:47 +0800

    Modify example.c

--- example.c
+++ example.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Rather class including article fact five shoulder side.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
